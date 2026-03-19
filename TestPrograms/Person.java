a05eaf29011c2767d1d73af17f3aca4d687ef822
Michel Bruecklmeier <michelbruecklmeier@aurora-schools.org>
2026-03-19 10:11:21 -0400

    Inital commit

---
+++ TestPrograms/Person.java
@@ -0,0 +1,41 @@
+//Simple java program to test reflection
+
+public class Person{
+    private int age;
+    private String firstName;
+    private String lastName;
+    //Simple constructor
+    public Person(int age, String firstName, String lastName){
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    //Testing constructor overloading
+    public Person(int age){
+        this(age,"Jhon","Doe");
+    }
+    public Person(){
+        this(100);
+    }
+    //Getter for name
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getFullName(){
+        return getFirstName() +" " + getLastName();
+    }
+    //Setters for name
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+}
---
+++ src/ObjectTester.java
@@ -0,0 +1,14 @@
+//This will actually load up the object being tested/the program
+
+
+public class ObjectTester {
+    //This will hold our current instance so we dont re run every time
+    private Object instance;
+    public ObjectTester(){
+
+    }
+    //Start the instance
+    public Object startInstance(){
+
+    }
+}
